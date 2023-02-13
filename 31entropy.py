3a5d4eb7ddf72809bb7d7bfd021cf0b187454285
Anber-Ansari <anber510@gmail.com>
2023-02-13 13:05:39 -0800

    init

--- 31entropy.py
+++ 31entropy.py
@@ -10,6 +10,12 @@
 
 # Hint: try breaking your program with erroneous input
 
+import math
+p = [0.1, 0.2, 0.3, 0.4] 	#probability distribution
+h = 0						#entropy of distribution
+for i in range(len(p)): 	#sets initial condition to 0 
+	h -= p[i] * math.log2(p[i]) #use (-) bc we want negative sum
+print(f'{h:.3f}')
 
 """
 python3 31entropy.py 0.1 0.2 0.3 0.4
