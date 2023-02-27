d1f874366b7a3de891cc39d403a5e502fafd12c1
Anber-Ansari <anber510@gmail.com>
2023-02-26 21:19:23 -0800

    init

--- 41transmembrane.py
+++ 41transmembrane.py
@@ -21,6 +21,50 @@
 # Hint: create a function for hydrophobic alpha-helix
 # Hint: use the same function for both signal peptide and transmembrane
 
+import mcb185
+import sys
+
+ 							
+def KD(seq):
+	h = 0
+	totalaa = 'ACDEFGHIKLMNPQRSTVWY'
+	for aa in totalaa:
+		if aa == 'A': h += 1.8
+		elif aa == 'C': h += 2.5
+		elif aa == 'D': h += -3.2
+		elif aa == 'E': h += 3.5
+		elif aa == 'F': h += 2.8
+		elif aa == 'G': h += -0.4
+		elif aa == 'H': h += -3.2
+		elif aa == 'I': h += 4.5
+		elif aa == 'K': h += -3.9
+		elif aa == 'L': h += 3.8
+		elif aa == 'M': h += 1.9
+		elif aa == 'N': h += -3.5
+		elif aa == 'P': h += -1.6
+		elif aa == 'Q': h += -3.5
+		elif aa == 'R': h += -4.5
+		elif aa == 'S': h += -0.8
+		elif aa == 'T': h += -0.7
+		elif aa == 'V': h += 4.2
+		elif aa == 'W': h += -0.9
+		else: 			h += -1.3
+	return h/len(seq)
+	
+def hah(seq, w, t):
+	for i in range(len(seq) -w +1):
+		win = seq[i:i+w]
+		if 'P' in win: continue
+		elif KD(win) > t: continue 
+		else:		return True
+	
+for defline, seq, in mcb185.read_fasta(sys.argv[1]):
+	if hah(seq[:30], 8, 2.5) and hah(seq[30:], 11, 2.0):
+		print(defline)
+	
+
+	
+	
 
 """
 python3 41transmembrane.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_protein.faa.gz
